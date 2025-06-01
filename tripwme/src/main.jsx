5cebb1a114e4466ecf2f6455f7156c92d4062388
Nerea MR <nerearojaas@gmail.com>
2025-06-01 19:58:53 +0200

    CORS cambiado

--- tripwme/src/main.jsx
+++ tripwme/src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import "./output.css";
-import './setupInterceptor';
+// import './setupInterceptor';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
