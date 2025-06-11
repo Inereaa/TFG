a060cffdc1eaa68f385d1de0d1cf7c4c407a8536
Nerea MR <nerearojaas@gmail.com>
2025-06-11 22:27:37 +0200

    intento de arreglo de bucle de peticiones

--- twm/public/index.php
+++ twm/public/index.php
@@ -1,8 +1,21 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// configuración de proxies y hosts de confianza
+if ($_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(
+        explode(',', $_SERVER['TRUSTED_PROXIES']),
+        Request::HEADER_X_FORWARDED_ALL
+    );
+}
+
+if ($_SERVER['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$_SERVER['TRUSTED_HOSTS']]);
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
